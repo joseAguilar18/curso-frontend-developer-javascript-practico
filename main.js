3a179c3f0aa50795709167708d8523ffb7da4f68
JoseAguilar <josefrainal200000@outlook.com>
2022-10-26 19:36:55 -0600

    Se agrego al index el menu mobile y tambien el carrito de compras, cada uno con sus respectivos comportamientos programados en js.

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoCompras);
+
 
 console.log(desktopMenu);
 
-function toggleDesktopMenu(event){
+function toggleDesktopMenu(){
+    aside.classList.add('inactive')
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){    
+    aside.classList.add('inactive');
+    mobileMenu.classList.toggle('inactive');
+    
+}
+
+function toggleCarritoCompras(){
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');    
+    desktopMenu.classList.add('inactive');
 }
